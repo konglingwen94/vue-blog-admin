50a56931103191dd7cd80ab41e5da4a886de1110
konglingwen94 <794430774@qq.com>
2020-10-28 12:31:22 +0800

    修改环境变量

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
+
 module.exports = {
-  // publicPath: process.env.NODE_ENV !== 'production' ? '/' : '/vue-blog-admin/dist/',
-  publicPath: process.env.NODE_ENV === "gh-pages" ? "/vue-blog-admin/" : "./",
+  publicPath: process.env.DEPLOY_PUBLICPATH === "gh-pages" ? "/vue-blog-admin/" : "./",
 
   devServer: {
     proxy: {
