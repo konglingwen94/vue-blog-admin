04004653ac073550323523d7a53fdc8a53ecb950
konglingwen94 <794430774@qq.com>
2020-10-28 10:54:34 +0800

    添加gh-pages 自动部署

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,15 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV !== 'production' ? '/' : '/vue-blog-admin/dist/',
-   
+  // publicPath: process.env.NODE_ENV !== 'production' ? '/' : '/vue-blog-admin/dist/',
+  publicPath: process.env.NODE_ENV === "gh-pages" ? "/vue-blog-admin/dist/" : "./",
+
   devServer: {
-
     proxy: {
-      '/api': {
-
-        target: 'http://localhost:8080',
+      "/api": {
+        target: "http://localhost:8080",
         pathRewrite(path, req) {
-
-           
-          return path.replace('/api', '/data') + '.json'
-
+          return path.replace("/api", "/data") + ".json";
         },
-
-      }
+      },
     },
   },
-}
+};
